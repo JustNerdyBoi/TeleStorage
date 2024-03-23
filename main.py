8daf7daa7bb0735ede9c68ff4ebe83b534c4420d
Fellcast <korivanplay@gmail.com>
2024-03-23 22:25:11 +0300

    registration fix

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, redirect
 from flask_restful import Api
 import resources
-
+from flask_login import LoginManager
 from data import db_session
+from data.user import User
 
 app = Flask(__name__, template_folder="static/htmls")
 app.secret_key = 'secretkeychangeonrelease'
 
 api = Api(app, catch_all_404s=True)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(id)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -29,7 +39,12 @@ def registration():
     if request.method == 'GET':
         return render_template('registration.html')
     elif request.method == 'POST':
-        print(request.form['email'])
+        user = User()
+        user.email = request.form['email']
+        user.hashed_password = request.form['password']
+        db_sess = db_session.create_session()
+        db_sess.add(user)
+        db_sess.commit()
         return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
