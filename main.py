6a988e719b0213acbd91925f88a42285dcb95450
Fellcast <korivanplay@gmail.com>
2024-03-23 20:21:10 +0300

    connecting shitfront

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 import resources
 
 from data import db_session
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="static/htmls")
 app.secret_key = 'secretkeychangeonrelease'
 
 api = Api(app, catch_all_404s=True)
 
 
+@app.route('/', methods=['POST', 'GET'])
+def landing():
+    return render_template('landing.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    return 'login page'
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def registration():
+    return render_template('registration.html')
+
+
+@app.route("/home", methods=['POST', 'GET'])
+def home():
+    return render_template('user-data_show.html')
+
+
 def main():
     db_session.global_init("db/base.db")  # инициация бд
-
-    api.add_resource(resources.Landing, '/')
-    api.add_resource(resources.Login, '/login')
-    api.add_resource(resources.Register, '/register')
-    api.add_resource(resources.Home, '/home')
-
-    app.run()
+    app.run(port=8000, host='127.0.0.1')
 
 
 if __name__ == '__main__':
